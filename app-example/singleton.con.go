de03712808f0fb80645c87fdad793d1e20854f37
devinluize <71080055+devinluize@users.noreply.github.com>
2024-04-05 10:11:24 +0700

    updating config for singleton connectipon

--- app-example/singleton.con.go
+++ app-example/singleton.con.go
@@ -1,7 +1,8 @@
-package app_example
+package app
 
 import (
 	"NewProjectTestingApi/helper"
+	"fmt"
 	"gorm.io/driver/sqlserver"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
@@ -10,7 +11,13 @@ import (
 var DB *gorm.DB
 
 func NewDB() {
-	dsn := "sqlserver://username:password@ip:port?database=databasename"
+	username := "database_username"
+	password := "database_password"
+	port := "1433"
+	database := "database name"
+	serverIP := "ip database"
+	// Create the DSN connection string
+	dsn := fmt.Sprintf("sqlserver://%s:%s@%s:%s?database=%s", username, password, serverIP, port, database)
 	db, err := gorm.Open(sqlserver.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
 	})
